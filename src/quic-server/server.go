685553866f2c091ca8843f9355d6840c6f01ee35
Alexander Færøy <ahf@torproject.org>
2018-02-10 18:13:34 +0100

    Add 2-clause BSD LICENSE file.

--- src/quic-server/server.go
+++ src/quic-server/server.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2018 Alexander Færøy. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Quic Pluggable Transport Server.
 package main
 
